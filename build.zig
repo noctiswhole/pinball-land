eb9420487875b513709c6537a6be36d39fcba67e
noctiswhole <anthony@creates.tech>
2025-12-06 19:34:47 -0600

    disable llvm for debug build

--- build.zig
+++ build.zig
@@ -22,7 +22,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "kirbys_pinball_land_dx_zig",
-        .use_llvm = true,
+        // .use_llvm = true,
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
@@ -56,7 +56,7 @@ pub fn build(b: *std.Build) void {
 
     const editor_exe = b.addExecutable(.{
         .name = "kirbys_pinball_land_dx_editor",
-        .use_llvm = true,
+        // .use_llvm = true,
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/editor.zig"),
             .target = target,
@@ -75,6 +75,10 @@ pub fn build(b: *std.Build) void {
     const editor_run_cmd = b.addRunArtifact(editor_exe);
     editor_run_step.dependOn(&editor_run_cmd.step);
     editor_run_cmd.step.dependOn(b.getInstallStep());
+    b.installArtifact(editor_exe);
+    editor_exe.addIncludePath(box2d.path("."));
+    editor_exe.linkLibrary(box2d.artifact("box2d"));
+    editor_exe.linkLibrary(raylib_artifact);
     b.installArtifact(editor_exe);
 
     // This allows the user to pass arguments to the application in the build
